5840f9cfa37d4070ed3945f687337a418fdf0486
Anthony <ant.osuant@gmail.com>
2025-11-09 23:52:12 -0500

    Add files via upload

---
+++ LAB 9 answers(Unzip to see) Second part added the target thing/Cannon.java
@@ -0,0 +1,48 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Cannon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+
+public class Cannon extends Actor
+{
+    private final static double CANNON_BALL_VELOCITY = 1500.0;
+    public void act()
+    {
+        // Add your action code here.
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        
+        if(mouse != null)
+        {
+            Vector2D cannonToMouse = new Vector2D(mouse.getX() - getX() , mouse.getY() - getY()); 
+
+            alignWithVector(cannonToMouse);
+
+            if (Greenfoot.mouseClicked(null))
+            {
+                cannonToMouse.normalize();
+                cannonToMouse = Vector2D.multiply(cannonToMouse, CANNON_BALL_VELOCITY);
+
+                CannonBall ball = new CannonBall();
+                ball.setVelocity(cannonToMouse);
+                getWorld().addObject(ball, getX(), getY());
+                Greenfoot.playSound("cannonSound.wav");
+            }
+
+        }
+    }
+
+    public void alignWithVector(Vector2D v){           
+        double adjacent = v.getX();
+        double opposite = v.getY();
+
+        double angleRadians = Math.atan2(opposite, adjacent);
+        double angleDegrees = Math.toDegrees(angleRadians);
+
+        setRotation((int) angleDegrees);  
+    }
+}
